2e3deed2b42865b26c1b254f01dac345b0c00a39
Ch3ru09 <xliang0905@gmail.com>
2023-11-23 23:15:27 -0500

    added grid

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,6 +1,9 @@
+import { redirect } from "react-router-dom";
 import styles from "./home.module.css";
 
 export default function Home() {
+  const pages = ["variables", "functions", "classes", "control-flow"];
+
   return (
     <>
       <header>
@@ -8,7 +11,15 @@ export default function Home() {
           What don't you <br /> understand?
         </h1>
       </header>
-      <main className={styles["grid"]}></main>
+      <main className={styles["grid"]}>
+        {pages.map((page) => {
+          return (
+            <div onClick={() => redirect(page)} className={styles["grid-item"]}>
+              {page}
+            </div>
+          );
+        })}
+      </main>
     </>
   );
 }
