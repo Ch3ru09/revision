5188a595a916c06a68052d813ccb73d3f8449373
Ch3ru09 <xliang0905@gmail.com>
2023-11-29 16:19:35 -0500

    testing removing preloader

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,12 +1,14 @@
 import { useNavigate } from "react-router-dom";
-import styles from "./home.module.css";
 import { Suspense } from "react";
+
 import Loading from "../components/loading/Loading";
+// import CodeBlock from "../components/codeblock/CodeBlock";
 import { icons } from "./icons";
-import CodeBlock from "../components/codeblock/CodeBlock";
+import styles from "./home.module.css";
 
 export default function Home() {
   const navigate = useNavigate();
+
   const LanguageSpecificPages = [
     "Fundamentals",
     "Variables",
@@ -19,14 +21,16 @@ export default function Home() {
 
   return (
     <>
-      <div style={{ display: "none" }}>
+      {/* <div style={{ display: "none" }}>
         <CodeBlock language="java">preloader</CodeBlock>
-      </div>
-      <header>
+      </div> */}
+
+      <header className={styles["title-container"]}>
         <h1 className={styles["title"]}>
           What don't you <br /> understand?
         </h1>
       </header>
+
       {[LanguageSpecificPages, AllLanguagePages].map((e, i) => {
         return (
           <section className={styles["grid"]} key={i}>
@@ -35,8 +39,7 @@ export default function Home() {
                 <div
                   onClick={() => navigate(page.toLowerCase())}
                   className={styles["grid-item"] + " hover-effect"}
-                  key={i}
-                >
+                  key={i}>
                   <Suspense fallback={<Loading />}>
                     <img src={icons[page.toLowerCase()]} />
                   </Suspense>
@@ -50,4 +53,3 @@ export default function Home() {
     </>
   );
 }
-
