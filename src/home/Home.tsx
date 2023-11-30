6f8c2134feecb1e3a5985f535b450d1a4af5d589
Ch3ru09 <xliang0905@gmail.com>
2023-11-29 21:50:50 -0500

    added parallax effect on home

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -4,10 +4,13 @@ import { Suspense } from "react";
 import Loading from "../components/loading/Loading";
 // import CodeBlock from "../components/codeblock/CodeBlock";
 import { icons } from "./icons";
+import useScrollPosition from "../components/hooks/useScrollPosition";
+
 import styles from "./home.module.css";
 
 export default function Home() {
   const navigate = useNavigate();
+  const titlePos = useScrollPosition(0.5);
 
   const LanguageSpecificPages = [
     "Fundamentals",
@@ -19,34 +22,44 @@ export default function Home() {
 
   const AllLanguagePages = ["Terminal"];
 
+  const sections = ["Language Specific", "All Languages"];
+
   return (
     <>
       {/* <div style={{ display: "none" }}>
         <CodeBlock language="java">preloader</CodeBlock>
       </div> */}
 
-      <header className={styles["title-container"]}>
-        <h1 className={styles["title"]}>
+      <header
+        className={styles["title-container"]}
+        style={{
+          translate: `0 ${titlePos}px`,
+          opacity: 1 - titlePos * 0.004,
+        }}>
+        <h1 className={styles["title"] + " not-selectable"}>
           What don't you <br /> understand?
         </h1>
       </header>
 
       {[LanguageSpecificPages, AllLanguagePages].map((e, i) => {
         return (
-          <section className={styles["grid"]} key={i}>
-            {e.map((page, i) => {
-              return (
-                <div
-                  onClick={() => navigate(page.toLowerCase())}
-                  className={styles["grid-item"] + " hover-effect"}
-                  key={i}>
-                  <Suspense fallback={<Loading />}>
-                    <img src={icons[page.toLowerCase()]} />
-                  </Suspense>
-                  <h2 className={styles["page-title"] + " home-h2"}>{page}</h2>
-                </div>
-              );
-            })}
+          <section className={styles["pages-container"]}>
+            <h2 className="home-h2">{sections[i]}</h2>
+            <div className={styles["grid"]} key={i}>
+              {e.map((page, i) => {
+                return (
+                  <div
+                    onClick={() => navigate(page.toLowerCase())}
+                    className={styles["grid-item"] + " hover-effect"}
+                    key={i}>
+                    <Suspense fallback={<Loading />}>
+                      <img src={icons[page.toLowerCase()]} />
+                    </Suspense>
+                    <h3 className={styles["page-title"]}>{page}</h3>
+                  </div>
+                );
+              })}
+            </div>
           </section>
         );
       })}
