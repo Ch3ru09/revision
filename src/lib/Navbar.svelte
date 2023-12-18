8d965efd27e866cf61e4149805532dd76adc5fe6
Ch3ru09 <xliang0905@gmail.com>
2023-12-18 18:33:01 -0500

    stated to add nav bar

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,29 @@
+<script>
+  let currentLanguage = "java";
+  const languages = ["Java", "Javascript", "Typescript", "Python", "Rust"];
+</script>
+
+<nav class="navbar">
+  <a href="/" class="home"> Home </a>
+  <div class="container">
+    <div class="languages">
+      <img
+        class="container-icon"
+        src={icons[language.toLowerCase()]}
+        alt={icons[language.toLowerCase()]}
+      />
+    </div>
+    <div class="dropdown">
+      <ul class="no-dot-ul">
+        {#each languages as language}
+          <li on:click={() => (currentLanguage = language)}>
+            <img class="icon" src={icons[language.toLowerCase()]} />
+          </li>
+        {/each}
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<style>
+</style>
