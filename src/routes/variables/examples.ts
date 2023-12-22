5bd4ac696c6425ee69821267b98328c2a764a64c
Ch3ru09 <xliang0905@gmail.com>
2023-12-21 23:45:51 -0500

    reformated variables examples

--- src/routes/variables/examples.ts
+++ src/routes/variables/examples.ts
@@ -1,28 +1,30 @@
 function getVariablesUses() {
-  return `import java.util.Scanner;
-
-public class Variables {
-  public static void main(String[] args) {
-
-    // as a counter
-    int index = 0;
-    index++; // > 1
-    index++; // > 2
-    
-    // as a reference
-    double final PI = 3.14159265;
-
-    // as an object that can be used
-    Scanner input = new Scanner(System.in);
-
-    // as a value value of an user input
-    String lastName = input.nextLine();
-
-    System.out.println(counter); // 2
-    System.out.println(PI); // 3.14159265;
-    System.out.println(lastName); // Liang
-  }
-}`;
+  return (
+    "import java.util.Scanner;\n" +
+    "\n" +
+    "public class Variables {\n" +
+    "\tpublic static void main(String[] args) {\n" +
+    "\n" +
+    "\t\t// as a counter\n" +
+    "\t\tint index = 0;\n" +
+    "\t\tindex++; // > 1\n" +
+    "\t\tindex++; // > 2\n" +
+    "\t\t\n" +
+    "\t\t// as a reference\n" +
+    "\t\tdouble final PI = 3.14159265;\n" +
+    "\n" +
+    "\t\t// as an object that can be used\n" +
+    "\t\tScanner input = new Scanner(System.in);\n" +
+    "\n" +
+    "\t\t// as a value value of an user input\n" +
+    "\t\tString lastName = input.nextLine();\n" +
+    "\n" +
+    "\t\tSystem.out.println(counter); // 2\n" +
+    "\t\tSystem.out.println(PI); // 3.14159265;\n" +
+    "\t\tSystem.out.println(lastName); // Liang\n" +
+    "\t}\n" +
+    "}"
+  );
 }
 
 function getFlowExamples() {
@@ -40,33 +42,37 @@ function getFlowExamples() {
 }
 
 function getDataTypeExamples() {
-  return `public class BuiltInDataTypes {
-  public static void main(String[] args) {
-    int age = 20;
-    float hourlySalary = 25.00f;
-    double LatitudePosition = 56.1304;
-    char grade = 'A';
-
-    String name = "foo";
-  }
-}`;
+  return (
+    "public class BuiltInDataTypes {\n" +
+    "\tpublic static void main(String[] args) {\n" +
+    "\t\tint age = 20;\n" +
+    "\t\tfloat hourlySalary = 25.00f;\n" +
+    "\t\tdouble LatitudePosition = 56.1304;\n" +
+    "\t\tchar grade = 'A';\n" +
+    "\n" +
+    '\t\tString name = "foo";\n' +
+    "\t}\n" +
+    "}"
+  );
 }
 
 function getVariableScopeExample() {
-  return `public class Scopes {
-  public static void main(String[] args) { // outer scope
-    int x = 1;
-
-    if (true) { // inner scope
-      int y = 2;
-      System.out.println(x); // works, prints 1
-      System.out.println(y); // works, prints 2
-    }
-      
-    System.out.println(x); // works, prints 1
-    System.out.println(y); // error: variable not initialized (variable not in scope)
-  }
-}`;
+  return (
+    "public class Scopes {\n" +
+    "\tpublic static void main(String[] args) { // outer scope\n" +
+    "\t\tint x = 1;\n" +
+    "\n" +
+    "\t\tif (true) { // inner scope\n" +
+    "\t\t\tint y = 2;\n" +
+    "\t\t\tSystem.out.println(x); // works, prints 1\n" +
+    "\t\t\tSystem.out.println(y); // works, prints 2\n" +
+    "\t\t}\n" +
+    "\n" +
+    "\t\tSystem.out.println(x); // works, prints 1\n" +
+    "\t\tSystem.out.println(y); // error: variable not initialized (variable not in scope)\n" +
+    "\t}\n" +
+    "}"
+  );
 }
 
 export { getVariablesUses, getFlowExamples, getDataTypeExamples, getVariableScopeExample };
